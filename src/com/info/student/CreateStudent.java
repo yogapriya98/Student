a2a6c9f6b18f19551cbe2970da01bb8efc0ac3cf
yogapriya <yogapriya.s98@gmail.com>
2018-06-02 16:59:59 +0530

    student project

---
+++ src/com/info/student/CreateStudent.java
@@ -0,0 +1,16 @@
+package com.info.student;
+
+public class CreateStudent {
+	public void printStudent(StudentDetails student) {
+		System.out.println(student);
+	}
+	public static void main(String args[]) {
+		StudentDetails student = new StudentDetails();
+		student.setFirstName("yoga");
+		student.setLastName("priya");
+		student.setGender("female");
+		student.setBranch("EEE");
+		CreateStudent printStudent = new CreateStudent();
+		printStudent.printStudent(student);
+}
+}
